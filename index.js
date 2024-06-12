c794e691e853484b8d40a01b729f06dfb584ac58
alfathalifdewa <alfathnaknetic@gmail.com>
2024-06-12 16:28:50 +0700

    Update API

--- index.js
+++ index.js
@@ -32,10 +32,7 @@ app.use('/order', orderRoutes);
 
 // Database Connection
 mongoose
-  .connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
+  .connect(process.env.MONGODB_URI)
   .then(() => console.log('MongoDB connected'))
   .catch((err) => console.log(err));
 
