ef680f98c5a845f72f13c4e38f62fbb2e7bd1438
Thorik <axe.army194@gmail.com>
2024-06-15 03:18:06 +0700

    add routes category on readme.md

--- README.md
+++ README.md
@@ -18,9 +18,12 @@ npm start
 | GET USER LOGIN | localhost:5000/users | GET | USER/ADMIN/SUPERADMIN |
 | GET PRODUCTS | localhost:5000/products | GET | USER/ADMIN/SUPERADMIN |
 | GET PRODUCTS ID | localhost:5000/products/:productName | GET | USER/ADMIN/SUPERADMIN |
-| SAVE PRODUCT | localhost:5000/products | POST | ADMIN/SUPERADMIN |
+| ADD PRODUCT | localhost:5000/products | POST | ADMIN/SUPERADMIN |
 | UPDATE PRODUCT | localhost:5000/products/:id | PUT | ADMIN/SUPERADMIN |
 | DELETE PRODUCT | localhost:5000/products/:id | DELETE | ADMIN/SUPERADMIN |
+| GET CATEGORY | localhost:5000/category | GET | USER/ADMIN/SUPERADMIN |
+| GET CATEGORY ID | localhost:5000/category/:id | GET | USER/ADMIN/SUPERADMIN |
+| ADD CATEGORY | localhost:5000/category | POST | SUPERADMIN |
 | GET CARTS | localhost:5000/cart | GET | USER/ADMIN/SUPERADMIN |
 | ADD CARTS | localhost:5000/cart | POST | USER/ADMIN/SUPERADMIN |
 | UPDATE CARTS | localhost:5000/cart/update | PUT | USER/ADMIN/SUPERADMIN |
--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,133 +1,140 @@
-import User from '../models/UserModel.js';
-import bcrypt from 'bcryptjs';
-import jwt from 'jsonwebtoken';
+import User from "../models/UserModel.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export const register = async (req, res) => {
-    const { username, email, password, img_url, phone } = req.body;
+  const { username, email, password, img_url, phone } = req.body;
 
-    try {
-        let user = await User.findOne({ username });
-        if (user) {
-            return res.status(400).json({ msg: 'User already exists' });
-        }
-
-        let emailCheck = await User.findOne({ email });
-        if (emailCheck) {
-            return res.status(400).json({ msg: 'Email already exists' });
-        }
-
-        user = new User({ username, email, password, img_url, phone });
-
-        const salt = await bcrypt.genSalt(10);
-        user.password = await bcrypt.hash(password, salt);
-
-        await user.save();
-        res.json({ msg : "User Registered" });
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).send('Server error');
+  try {
+    let user = await User.findOne({ username });
+    if (user) {
+      return res.status(400).json({ msg: "User already exists" });
     }
+
+    let emailCheck = await User.findOne({ email });
+    if (emailCheck) {
+      return res.status(400).json({ msg: "Email already exists" });
+    }
+
+    user = new User({ username, email, password, img_url, phone });
+
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(password, salt);
+
+    await user.save();
+    res.json({ msg: "User Registered" });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
 };
 
 export const login = async (req, res) => {
-    const { username, password } = req.body;
+  const { username, password } = req.body;
 
-    try {
-        let user = await User.findOne({ username });
-        if (!user) {
-            return res.status(400).json({ msg: 'Invalid Credentials' });
-        }
-
-        const isMatch = await bcrypt.compare(password, user.password);
-        if (!isMatch) {
-            return res.status(400).json({ msg: 'Invalid Credentials' });
-        }
-
-        const payload = { user: { id: user.id, role: user.role } };
-        jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '1h' }, (err, token) => {
-            if (err) throw err;
-            res.json({ token, user:{
-                username
-            } });
-        });
-
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).send('Server error');
+  try {
+    let user = await User.findOne({ username });
+    if (!user) {
+      return res.status(400).json({ msg: "Invalid Credentials" });
     }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ msg: "Invalid Credentials" });
+    }
+
+    const payload = { user: { id: user.id, role: user.role } };
+    jwt.sign(
+      payload,
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" },
+      (err, token) => {
+        if (err) throw err;
+        res.json({
+          token,
+          user: {
+            username,
+          },
+        });
+      }
+    );
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
 };
 
 export const getUsers = async (req, res) => {
-    try {
-        const users = await User.findById(req.user.id).select('-password');
-        res.json(users);
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).send('Server error');
-    }
+  try {
+    const users = await User.findById(req.user.id).select("-password");
+    res.json(users);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
 };
 
 export const getAllUsers = async (req, res) => {
-    try {
-        const allUsers = await User.find().select('-password');
-        res.json(allUsers);
-    } catch (error) {
-        console.error(err.message);
-        res.status(500).send('Server error');
-    }
-}
+  try {
+    const allUsers = await User.find().select("-password");
+    res.json(allUsers);
+  } catch (error) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+};
 
 export const updateUser = async (req, res) => {
-    const { username, email, img_url, phone, password } = req.body;
+  const { username, email, img_url, phone, password } = req.body;
 
-    try {
-        let user = await User.findById(req.user.id);
-        if (!user) {
-            return res.status(404).json({ msg: 'User not found' });
-        }
-
-        // Check if the username is being updated and if it's already taken
-        if (username && username !== user.username) {
-            const usernameExists = await User.findOne({ username });
-            if (usernameExists) {
-                return res.status(400).json({ msg: 'Username already exists' });
-            }
-            user.username = username;
-        }
-
-        // Check if the email is being updated and if it's already taken
-        if (email && email !== user.email) {
-            const emailExists = await User.findOne({ email });
-            if (emailExists) {
-                return res.status(400).json({ msg: 'Email already exists' });
-            }
-            user.email = email;
-        }
-
-        if (img_url) user.img_url = img_url;
-        if (phone) user.phone = phone;
-
-        if (password) {
-            const salt = await bcrypt.genSalt(10);
-            user.password = await bcrypt.hash(password, salt);
-        }
-
-        await user.save();
-        res.json({ msg: 'User updated successfully', user });
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).send('Server error');
+  try {
+    let user = await User.findById(req.user.id);
+    if (!user) {
+      return res.status(404).json({ msg: "User not found" });
     }
+
+    // Check if the username is being updated and if it's already taken
+    if (username && username !== user.username) {
+      const usernameExists = await User.findOne({ username });
+      if (usernameExists) {
+        return res.status(400).json({ msg: "Username already exists" });
+      }
+      user.username = username;
+    }
+
+    // Check if the email is being updated and if it's already taken
+    if (email && email !== user.email) {
+      const emailExists = await User.findOne({ email });
+      if (emailExists) {
+        return res.status(400).json({ msg: "Email already exists" });
+      }
+      user.email = email;
+    }
+
+    if (img_url) user.img_url = img_url;
+    if (phone) user.phone = phone;
+
+    if (password) {
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+    }
+
+    await user.save();
+    res.json({ msg: "User updated successfully", user });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
 };
 
 export const logout = async (req, res) => {
-    try {
-        // You can use token blacklisting or simply clear the token from the client side
-        res.clearCookie('token'); // Clear the token cookie
-        res.json({ msg: 'Logout successful' });
-        document.cookie = "token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).send('Server error');
-    }
+  try {
+    // You can use token blacklisting or simply clear the token from the client side
+    res.clearCookie("token"); // Clear the token cookie
+    res.json({ msg: "Logout successful" });
+    document.cookie = "token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
 };
